0378b755b06daed29c55bec5de5ed0443c9f15b2
kot Deng <kotdeng666@gmail.com>
2018-06-02 15:57:08 +0800

    没修改啥

--- assets/script/ListDemoItem.ts
+++ assets/script/ListDemoItem.ts
@@ -10,13 +10,17 @@ export class ListDemoItem extends ListItemBase {
     protected indexLabel: cc.Label;
     @property({ type: cc.Label })
     protected sizeLabel: cc.Label;
+    @property({ type: cc.Node })
+    protected back: cc.Node;
+
 
     public setData(data: cc.Vec2, index: number): void {
         super.setData(data, index);
         this.indexLabel.string = index.toString();
         this.sizeLabel.string = data.y.toString();
-        this.node.width = data.x;
+        // this.node.width = data.x;
         this.node.height = data.y;
+        this.back.height = data.y - 4;
         return;
     }
 
